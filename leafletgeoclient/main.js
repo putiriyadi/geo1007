2006236aefd1a8bca60f6c81e22ec2341c998079
Puti Riyadi <putiriyadi@gmail.com>
2023-05-19 10:36:42 +0200

    add leaflet folder new

--- leafletgeoclient/main.js
+++ leafletgeoclient/main.js
@@ -108,3 +108,25 @@ function onMapClick(e) {
 }
 
 map.on('click', onMapClick);
+
+
+// add geolocation
+
+map.locate({setView: true, maxZoom: 16});
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
+
